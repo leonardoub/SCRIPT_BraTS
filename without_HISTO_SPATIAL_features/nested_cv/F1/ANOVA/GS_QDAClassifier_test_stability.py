5da6a2ecd2d48e15ced50fb75d9761d93f386183
leonardoub <ubaldi.leonardo@gmail.com>
2020-04-26 09:57:00 +0200

    grt boost

---
+++ without_HISTO_SPATIAL_features/nested_cv/F1/ANOVA/GS_QDAClassifier_test_stability.py
@@ -0,0 +1,49 @@
+#Cross Validation on QuadraticDiscriminantAnalysis for classification
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import GridSearchCV 
+from sklearn.model_selection import RandomizedSearchCV
+import load_data
+import save_output
+import nested_cv
+from sklearn.feature_selection import SelectKBest, SelectPercentile
+from sklearn.feature_selection import f_classif
+import save_features_selected_ANOVA
+
+name = 'QDAClassifier'
+dim_reduction = 'ANOVA'
+
+#load data
+
+public_data, public_labels = load_data.function_load_data()
+
+#Scalers
+
+from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler
+scalers_to_test = [StandardScaler(), RobustScaler(), MinMaxScaler(), None]
+
+#QuadraticDiscriminantAnalysis
+steps = [('scaler', MinMaxScaler()), ('red_dim', SelectPercentile(f_classif, percentile=10)), ('clf', QuadraticDiscriminantAnalysis())]
+
+pipeline = Pipeline(steps)
+
+parameteres = [{'scaler':scalers_to_test}]
+
+results, dict_best_estimators = nested_cv.function_nested_cv(public_data, public_labels, pipeline, parameteres)
+
+#create and save file best features ANOVA
+
+tot_features = public_data.columns
+save_features_selected_ANOVA.function_save_features_selected_ANOVA(dim_reduction, name, tot_features, dict_best_estimators)
+
+#create folder and save
+
+save_output.function_save_output(results, dim_reduction, name)
+
---
+++ without_HISTO_SPATIAL_features/nested_cv/F1/None/GS_KNeighborsClassifier_test_stability.py
@@ -0,0 +1,52 @@
+#Cross Validation on KNeighborsClassifier for classification
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import GridSearchCV 
+from sklearn.model_selection import RandomizedSearchCV
+import load_data
+import save_output
+import nested_cv
+
+
+name = 'KNeighbors'
+dim_reduction = 'NONE'
+
+#load data
+
+public_data, public_labels = load_data.function_load_data()
+
+#Scalers
+
+from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler
+scalers_to_test = [StandardScaler(), RobustScaler(), MinMaxScaler(), None]
+
+
+# Designate distributions to sample hyperparameters from 
+n_features_to_test = np.arange(1,11)
+k = np.arange(1,11)
+
+#KNeighborsClassifier
+steps = [('scaler', MinMaxScaler()), ('clf', KNeighborsClassifier())]
+
+pipeline = Pipeline(steps)
+
+parameteres = [{'scaler':scalers_to_test, 'clf__n_neighbors':k, 
+              'clf__weights':['uniform', 'distance'], 'clf__algorithm':['auto', 'ball_tree', 'kd_tree', 'brute']}]
+
+
+results = nested_cv.function_nested_cv(public_data, public_labels, pipeline, parameteres)
+
+#create folder and save
+
+save_output.function_save_output(results, dim_reduction, name)
+
+       
