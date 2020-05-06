649a6fd8cf9d70d677e5b0404f8bc09ab5d9f874
leonardoub <ubaldi.leonardo@gmail.com>
2020-05-06 22:44:59 +0200

    corr

--- without_HISTO_SPATIAL_features/score_cv/ROC_AUC_optimization/select_best_features/best_features_for_svm/save_output.py
+++ without_HISTO_SPATIAL_features/score_cv/ROC_AUC_optimization/select_best_features/best_features_for_svm/save_output.py
@@ -4,7 +4,7 @@ import pandas as pd
 def function_save_output(final_data, dimensionality_reduction, name_clf):
     outname = f'features_importance_for_{name_clf}_best_HP_dim_red_{dimensionality_reduction}.csv'
 
-    outdir = f'/home/users/ubaldi/TESI_BRATS/result_score/data_without_HISTO_SPATIAL/score_ROC_AUC_optimization/{name_clf}_stability/best_feature_importances_for_{name_clf}'
+    outdir = f'/home/users/ubaldi/TESI_BRATS/important_features/data_without_HISTO_SPATIAL/score_ROC_AUC_optimization/{name_clf}_stability/best_feature_importances_for_{name_clf}'
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
